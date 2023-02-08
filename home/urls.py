fbdc01907e4e9a7851e7b04b95fcaf4baffd5243
rajesh-sht <shrestharajesh.sr@gmail.com>
2023-02-08 15:05:08 +0545

    homepage finished

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from .views import *
 from django.urls import path
 
-urlpatterns = {
-    path('', HomeView.as_view(), name='home')
-}
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('brand/<slug>', BrandView.as_view(), name='brand'),
+]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import View
 from .models import *
 # Create your views here.
 
 class BaseView(View):
     views = {}
+    views['categories'] = Category.objects.all()
 
 class HomeView(BaseView):
     def get(self,request):
+        self.views
+        self.views['subcategories'] = SubCategory.objects.all()
+        self.views['sliders'] = Slider.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['brands'] = Brand.objects.all()
+        self.views['reviews'] = Review.objects.all()
+        self.views['new_products'] = Product.objects.filter(labels = 'new')
+        self.views['hot_products'] = Product.objects.filter(labels = 'hot')
+        self.views['sale_products'] = Product.objects.filter(labels = 'sale')
 
-        return render(request, 'index.html')
+        return render(request, 'index.html',self.views)
+
+
+class CategoryView(BaseView):
+    def get(self,request,slug):
+        ids = Category.objects.get(slug = slug).id
+        self.views['category_products'] = Product.objects.filter(category_id = ids)
+        return render(request, 'category.html', self.views)
+
+class BrandView(BaseView):
+    def get(self,request,slug):
+        ids = Brand.objects.get(slug = slug).id
+        self.views['brand_products'] = Product.objects.filter(brand_id = ids)
+        return render(request, 'brand.html', self.views)
